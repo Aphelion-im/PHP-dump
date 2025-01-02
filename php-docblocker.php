82be5e2fe0286f4dbc19c616d43adaca481ebfef
Aphelion Web Development <17068516+Aphelion-im@users.noreply.github.com>
2025-01-02 16:42:43 +0100

    Add files via upload

---
+++ php-docblocker.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @function addParams
+ * Add two numbers
+ * 
+ * @param int $param1
+ * @param int $param2
+ * @return int
+ */
+function addParams($param1, $param2) {
+  return $param1 + $param2;
+}
+
+echo addParams(1, 2) . "<br />";
+echo addParams(4, 6);
